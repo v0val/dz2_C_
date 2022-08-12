b81a2ea9397de381349cda37e182a3e220e5a983
valeryO <olennval@mail.ru>
2022-08-12 19:40:45 +0700

    number 19

---
+++ N19/Program.cs
@@ -0,0 +1,19 @@
+﻿m1:
+Console.WriteLine("Введите 5-ти значное число");
+string? a = Console.ReadLine();
+if (a.Length != 5)
+{
+Console.WriteLine("число не является пятизначным, попробуйте еще раз");
+goto m1;
+}
+else
+{
+if (a[0] == a[4] && a[1] == a[3])
+{
+Console.WriteLine("число палиндром");
+}
+else
+{
+Console.WriteLine("число не палиндром");
+}
+}
