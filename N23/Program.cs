f6586dd15e7f08eeb4dde689d793fa6da6c41746
valeryO <olennval@mail.ru>
2022-08-12 23:15:32 +0700

    number 23

---
+++ N23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Ввведите натуральное число");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i < N+1; i++)
+{
+Console.Write( $"{i} ^ 3 = " );
+Console.WriteLine(Math.Pow(i, 3));
+}
